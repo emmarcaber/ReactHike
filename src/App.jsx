dc71eae4387a2bcb7d96a4747ae012f3311391e5
Emmar Caber <caberemmar@gmail.com>
2024-01-08 16:19:58 +0800

    feat: add external css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Card from './Card.jsx'
+import Button from './Button.jsx'
 
 function App() {
   
   return (
     <>
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Button />
     </>
   );
 }
