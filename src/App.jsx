1f6cda9a91b0b15b06f4f3a58ad2378747565319
Emmar Caber <caberemmar@gmail.com>
2024-01-08 20:31:04 +0800

    refactor: rename button component

--- src/App.jsx
+++ src/App.jsx
@@ -2,68 +2,8 @@ import List from "./List.jsx";
 
 function App() {
   
-  const fruits = [
-    {
-        id: 1,
-        name: "Apple",
-        calories: 95
-    }, 
-    {
-        id: 2,
-        name: "Orange",
-        calories: 45
-    }, 
-    {
-        id: 3,
-        name: "Banana",
-        calories: 105
-    }, 
-    {
-        id: 4,
-        name: "Coconut",
-        calories: 159
-    }, 
-    {
-        id: 5,
-        name: "Pineapple",
-        calories: 37
-    }];
-
-  const vegetables = [
-    {
-        id: 6,
-        name: "Potatoes",
-        calories: 110
-    }, 
-    {
-        id: 7,
-        name: "Celery",
-        calories: 15
-    }, 
-    {
-        id: 8,
-        name: "Carrots",
-        calories: 25
-    }, 
-    {
-        id: 9,
-        name: "Corn",
-        calories: 63
-    }, 
-    {
-        id: 10,
-        name: "Brocolli",
-        calories: 50
-    }];
-
   return (
     <>
-      {fruits.length > 0 &&
-        <List items={fruits} category="Fruits" />
-      }
-      {vegetables.length > 0 &&
-        <List items={vegetables} category="Vegetables" />
-      };
     </>
   );
 }
