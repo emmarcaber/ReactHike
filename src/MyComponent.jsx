8d11a8bdf42636e16f584c10d9db5e7b2b753758
Emmar Caber <caberemmar@gmail.com>
2024-01-08 23:44:55 +0800

    feat: toggle boolean using useState function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function MyComponent() {
     const [name, setName] = useState("Guest");
     const [age, setAge] = useState(0);
+    const [isEmployed, setIsEmployed] = useState(false);
 
     const updateName = () => {
         setName("Spongebob")
@@ -12,6 +13,10 @@ function MyComponent() {
         setAge(age + 1)
     }
 
+    const toggleEmployedStatus = () => {
+        setIsEmployed(!isEmployed);
+    }
+
     return (
         <div>
             <p>Name: {name}</p>
@@ -19,6 +24,9 @@ function MyComponent() {
 
             <p>Age: {age}</p>
             <button onClick={incrementAge}>Increment Age</button>
+
+            <p>Employed: {isEmployed ? "Yes" : "No"}</p>
+            <button onClick={toggleEmployedStatus}>Toggle Employed Status</button>
         </div>
     )
 }
