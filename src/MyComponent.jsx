afb2ab684f8ae68686c092daa4755566ac5ab559
Emmar Caber <caberemmar@gmail.com>
2024-01-08 23:42:21 +0800

    feat: increment a number using useState function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 
 function MyComponent() {
-    const [name, setName] = useState();
+    const [name, setName] = useState("Guest");
+    const [age, setAge] = useState(0);
 
     const updateName = () => {
         setName("Spongebob")
+    }
+
+    const incrementAge = () => {
+        setAge(age + 1)
     }
 
     return (
         <div>
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
+
+            <p>Age: {age}</p>
+            <button onClick={incrementAge}>Increment Age</button>
         </div>
     )
 }
