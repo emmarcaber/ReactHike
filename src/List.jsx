66d2d4985a8bbb2adbea8b2622782efedb2f0e98
Emmar Caber <caberemmar@gmail.com>
2024-01-08 18:45:38 +0800

    feat: reusable List component

--- src/List.jsx
+++ src/List.jsx
@@ -1,50 +1,21 @@
 
-function List() {
-    const fruits = [
-    {
-        id: 1,
-        name: "Apple",
-        calories: 95
-    }, 
-    {
-        id: 2,
-        name: "Orange",
-        calories: 45
-    }, 
-    {
-        id: 3,
-        name: "Banana",
-        calories: 105
-    }, 
-    {
-        id: 4,
-        name: "Coconut",
-        calories: 159
-    }, 
-    {
-        id: 5,
-        name: "Pineapple",
-        calories: 37
-    }];
+function List(props) {
+
+    const category = props.category;
+    const itemList = props.items;
     
-    // SORTING THE LISTS
-    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // ALPHABETICAL
-    // fruits.sort((a, b) => b.name.localeCompare(a.name)); // REVERSE ALPHABETICAL
-    // fruits.sort((a, b) => a.calories - b.calories); //  NUMERIC
-    fruits.sort((a, b) => b.calories - a.calories); //  NUMERIC
-
-    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
-    const highCalFruits = fruits.filter(fruit => fruit.calories >=  100);
-
-    const listItems = highCalFruits.map(highCalFruit => 
-        <li key={highCalFruit.id}>
-            {highCalFruit.name}: &nbsp;
-            <b>{highCalFruit.calories}</b>
+    const listItems = itemList.map(item => 
+        <li key={item.id}>
+            {item.name}: &nbsp;
+            <b>{item.calories}</b>
         </li>
     );
 
     return (
-        <ol>{listItems}</ol>
+        <>
+            <h3>{category}</h3>
+            <ol>{listItems}</ol>
+        </>
     );
 }
 
