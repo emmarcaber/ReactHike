7b884cc090f445023796253120dadb708d707bb0
Emmar Caber <caberemmar@gmail.com>
2024-01-09 00:04:52 +0800

    feat: onChange event on input[type=text]

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -2,15 +2,23 @@ import React, {useState} from "react";
 
 function MyComponent() {
     const [name, setName] = useState("Guest");
+    const [quantity, setQuantity] = useState(0);
 
     function handleNameChange(e) {
         setName(e.target.value);
     }
 
+    function handleQuantityChange(e) {
+        setQuantity(e.target.value);
+    }
+
     return (
         <div>
-            <input value={name} onChange={handleNameChange} />
+            <input type="text" value={name} onChange={handleNameChange} />
             <p>{name}</p>
+
+            <input type="number" value={quantity} onChange={handleQuantityChange} />
+            <p>{quantity}</p>
         </div>
     )
 }
