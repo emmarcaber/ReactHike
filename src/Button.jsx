be3b4a250a68e2785cfa325108c5acd778a9b4b5
Emmar Caber <caberemmar@gmail.com>
2024-01-08 20:48:48 +0800

    feat: button onclick events with condition

---
+++ src/Button.jsx
@@ -0,0 +1,20 @@
+
+function Button() {
+
+    let count = 0;
+
+    const handleClick = (name) => {
+        if (count < 3) {
+            count++;
+            console.log(`${name} you clicked me ${count} times(s)`)
+        } else {
+            console.log(`${name} stop clicking me!`)
+        }
+    } 
+
+    return (
+        <button onClick={() => handleClick("Emmar Caber")}>Click me! </button>
+    );
+} 
+
+export default Button;
